85861bac8eec869e1ee4636bda4363faf9e19e21
Julian Dawson <julian@juliandawson.co.uk>
2017-04-19 22:17:54 +0100

    Responsive nav mostly done. Need to tidy

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,11 @@
-$(".js-nav-menu").click(function (event) {
+$(".js-page-menu-trigger").click(function (event) {
   event.preventDefault();
 
-  // change page-head color to match sidebar
-  $(".c-page-head").toggleClass("has-fixed-nav-menu");
-
   // toggle sidebar open and closed
-  $(".c-sidebar").toggleClass("is-open");
+  $(".c-page-menu").toggleClass("is-open");
 
   // toggle 'Menu' and 'Close' text
-  $(".js-nav-menu-text").text(function (i, text) {
+  $(this).text(function (i, text) {
     return text === "Menu" ? "Close" : "Menu";
   });
 });

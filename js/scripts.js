e69127750f7828fbeb954e79bf297bfd90794c41
Julian Dawson <julian@juliandawson.co.uk>
2017-04-20 01:04:13 +0100

    Resize default header and add dynamic resize

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,25 @@
 $(".js-page-menu-trigger").click(function (event) {
   event.preventDefault();
 
+  var $this = $(this);
+  var $menu = $(".c-page-menu");
+
   // toggle sidebar open and closed
-  $(".c-page-menu").toggleClass("is-open");
+  $menu.toggleClass("is-open");
 
   // toggle 'Menu' and 'Close' text
-  $(this).text(function (i, text) {
+  $this.text(function (i, text) {
     return text === "Menu" ? "Close" : "Menu";
   });
+});
+
+$(window).scroll(function () {
+  var $this = $(this);
+  var $head = $('.c-page-head');
+
+  if ($(window).scrollTop() < 96) {
+    $head.addClass('c-page-head--large');
+  } else {
+    $head.removeClass('c-page-head--large');
+  }
 });

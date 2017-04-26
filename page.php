4424a2ceb884f81adcb50727bd666afed739ee8e
Julian Dawson <julian@juliandawson.co.uk>
2017-04-26 17:30:59 +0100

    Use has_post_thumbnail over masthead templates

--- page.php
+++ page.php
@@ -1,29 +1,34 @@
 <?php
 /**
- * @desc A page. See single.php for blog post layout.
+ * The default page template for displaying all pages.
+ *
+ * @link https://github.com/carlislephoto/carlislephoto-org-uk-theme
+ *
+ * @package WordPress
+ * @subpackage CarlislePhoto
  */
-?>
 
-<?php get_header(); ?>
+get_header();
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+while (have_posts()) : the_post();
+  
+  if( has_post_thumbnail() ) : ?>
+  <div class="c-masthead">
+    <figure class="c-masthead__media">
+      <?php the_post_thumbnail(); ?>
+    </figure>
+  </div>
+  <?php endif; ?>
 
-<main class="c-blog-page s-blog-page">
-  <div class="c-band">
-    <div class="o-wrapper">
-      <div class="c-blog-page__content">
-
-        <?php the_content(__('(more...)')); ?>
-
+  <main class="c-blog-page s-blog-page">
+    <div class="c-band">
+      <div class="o-wrapper">
+        <div class="c-blog-page__content">
+          <?php the_content(); ?>
+        </div>
       </div>
     </div>
-  </div>
-</main>
+  </main>
 
-<?php endwhile; else: ?>
-
-  <p>Sorry, no pages matched your criteria.</p>
-
-<?php endif; ?>
-
-<?php get_footer(); ?>
+<?php endwhile;
+get_footer(); ?>
--- template-bare.php
+++ template-bare.php
@@ -1,23 +1,28 @@
 <?php
 /**
  * Template Name: Template Bare
+ *
+ * @link https://github.com/carlislephoto/carlislephoto-org-uk-theme
+ *
+ * @package WordPress
+ * @subpackage CarlislePhoto
  */
-?>
 
-<?php get_header(); ?>
+get_header();
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+while (have_posts()) : the_post();
+  
+  if( has_post_thumbnail() ) : ?>
+  <div class="c-masthead">
+    <figure class="c-masthead__media">
+      <?php the_post_thumbnail(); ?>
+    </figure>
+  </div>
+  <?php endif; ?>
 
-<main class="c-blog-page s-blog-page">
+  <main class="c-blog-page s-blog-page">
+    <?php the_content(); ?>
+  </main>
 
-  <?php the_content(__('(more...)')); ?>
-
-</main>
-
-<?php endwhile; else: ?>
-
-  <p>Sorry, no pages matched your criteria.</p>
-
-<?php endif; ?>
-
-<?php get_footer(); ?>
+<?php endwhile;
+get_footer(); ?>
